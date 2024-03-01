940ba80146a1cc20210b3cb2d13203242f31db62
Bibhu Prasad <bibhup.behera@nettantra.net>
2024-03-01 17:04:21 +0530

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@components/layout/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="relative">
+          <Navbar className="top-2" />
+          <div className="mt-24">
+            {children}
+          </div>
+        </div>
+      </body>
     </html>
   );
 }
